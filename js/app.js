0c328d6b6b2861ed616ca802f2c37858ae4db4d1
Tomas Gaitan <tomasagustingaitan@gmai.com>
2022-02-22 01:09:23 -0300

    Subida de archivos

---
+++ js/app.js
@@ -0,0 +1,16 @@
+window.addEventListener('load',()=>{
+
+    /* const row = document.getElementById('row');
+    const btn = document.getElementById('btn')
+
+    btn.addEventListener('click',()=>{
+        if (row.classList.contains('row-cols-4')){
+            row.classList.remove('row-cols-4');
+            row.classList.add('row-cols-2');
+        } else {
+            row.classList.remove('row-cols-2');
+            row.classList.add('row-cols-4');
+        }
+        
+    }) */
+})
